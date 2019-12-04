89fd8194cd78a6c99559cf739659bbeeb1ba4987
hirotaka.iwasaki <nddgs@yahoo.co.jp>
2019-12-04 14:38:12 +0900

    201912041438

--- app/src/main/java/test/test191204n02_payjp_n01/MainActivity.kt
+++ app/src/main/java/test/test191204n02_payjp_n01/MainActivity.kt
@@ -1,6 +1,7 @@
 package test.test191204n02_payjp_n01
 
-import android.support.v7.app.AppCompatActivity
+//import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
