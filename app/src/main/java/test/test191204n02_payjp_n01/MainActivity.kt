e30f9106ef7788a6023a19cc666edfc8a08314a0
hirotaka.iwasaki <nddgs@yahoo.co.jp>
2019-12-04 13:01:00 +0900

    Initial commit

---
+++ app/src/main/java/test/test191204n02_payjp_n01/MainActivity.kt
@@ -0,0 +1,12 @@
+package test.test191204n02_payjp_n01
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
